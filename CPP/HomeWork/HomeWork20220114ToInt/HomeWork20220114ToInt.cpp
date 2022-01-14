393beb137071552128cb943ac33cddbd36edceba
기배 김 <dkqjswk789@naver.com>
2022-01-14 18:24:30 +0900

    김기배 : 20220114ToInt

---
+++ CPP/HomeWork/HomeWork20220114ToInt/HomeWork20220114ToInt.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+int ToInt(const char* _Left)
+{
+    int Value = 0;
+    int len = 0;
+    while ('\0' != _Left[len])
+    {
+        len += 1;
+    }
+
+    //"12" (10*1)+(10*0*1)
+    //_Left[1]=='2'
+
+    for (int i = 0; i < len; i += 1)
+    {
+        //자릿수
+        int digit = 1;
+        for (int j = i; j < len - 1; j += 1)
+        {
+            digit *= 10;
+        }
+        Value += ((_Left[i] - 48) * digit);
+    }
+    return Value;
+}
+int main()
+{
+    int Value = ToInt("123456");
+    char test = '0';
+    test -= 48;
+    test;
+}
