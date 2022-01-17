aed511f38a76ec9008a83c395cfb8b7fc5dcb026
기배 김 <dkqjswk789@naver.com>
2022-01-18 05:31:53 +0900

    김기배 : 2022 01 18 CountEnd 수정

--- CPP/HomeWork/HomeWork20220117CountEnd/CountEnd.cpp
+++ CPP/HomeWork/HomeWork20220117CountEnd/CountEnd.cpp
@@ -12,6 +12,14 @@ int FindLen(const char* _Text)
 //전체 텍스트,시작 위치,찾을 텍스트
 int CountEnd(const char* _Text, int _End, const char* _FindStr)
 {
+	//최대길이 넘길시 예외처리 
+	int len = FindLen(_Text)-1;
+	if (_End > len)
+	{
+		_End = len;
+	}
+
+
 	int Search_Text_Len = FindLen(_FindStr);
 	for (int i = _End; i >= 0; i -= 1)
 	{
@@ -32,5 +40,5 @@ int CountEnd(const char* _Text, int _End, const char* _FindStr)
 }
 int main()
 {
-	int Count = CountEnd("aaa ttt eee asdfasd", 50, "eee");
+	int Count = CountEnd("aaa ttt eee asdfasd", 50, "asdfasd");
 }
