4744e855341e9ac4a24077f4b204bd65ab4819a1
기배 김 <dkqjswk789@naver.com>
2022-02-25 05:17:25 +0900

    김기배:HomeWork2022-02-25 GameMapLeak

--- CPP/HomeWork/HomeWork20220222GameVectorSort/HomeWork20220222GameVectorSort.cpp
+++ CPP/HomeWork/HomeWork20220222GameVectorSort/HomeWork20220222GameVectorSort.cpp
@@ -7,12 +7,12 @@ int main()
 	MyVector.push_back(10);
 	MyVector.push_back(5);
 	MyVector.push_back(1);
-	MyVector.push_back(2);
-	MyVector.push_back(8);
-	MyVector.push_back(99);
-	MyVector.push_back(0);
-	MyVector.push_back(1);
-
+	//MyVector.push_back(2);
+	//MyVector.push_back(8);
+	//MyVector.push_back(99);
+	//MyVector.push_back(0);
+	//MyVector.push_back(1);
+	//MyVector.push_back(3);*/
 
 	for (int i = 0; i < MyVector.size(); ++i)
 	{
