965492bed9e00834603fd4f12a89543fb6ba80bf
기배 김 <dkqjswk789@naver.com>
2022-03-04 01:54:45 +0900

    김기배:2022-03-04 API 2일

---
+++ API/GameApp/main.cpp
@@ -0,0 +1,10 @@
+#include <Windows.h>
+#include <GameEngineBase/GameEngineWindow.h>
+int __stdcall WinMain(_In_ HINSTANCE hInstance,
+    _In_opt_ HINSTANCE hPrevInstance,
+    _In_ LPSTR    lpCmdLine,
+    _In_ int       nCmdShow)
+{
+    GameEngineWindow::GetInst().CreateGameWindow(hInstance);
+    GameEngineWindow::GetInst().ShowGameWindow();
+}
